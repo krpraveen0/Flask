80b5d0cfd84796536323524433c6492ce811a239
pravee kumar <pkec151015@gmail.com>
2020-08-15 22:44:33 +0530

    debug mode

--- flask-app/app.py
+++ flask-app/app.py
@@ -4,15 +4,35 @@ from flask import Flask
 
 #create the application object
 app = Flask(__name__)
+app.config["DEBUG"]= True
 
 #use the decorator pattern to link 
 #the view function to  a url
-@app.route("/")
-@app.route("/hello")
 
 #define the view using a function , which return a string 
-def hello_world():
-    return "HEllo World"
+@app.route("/")
+@app.route("/integer/<int:value>")
+def int_type(value):
+    print(value + 1)
+    return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+    print(value + 1)
+    return  "correct"
+
+@app.route("/path/<path:value>")
+def path_type(value):
+    print(value)
+    return "correct"
+
+@app.route("/name/<name>")
+def index(name):
+    if name.lower() == "naveen":
+        return "hello, {}".format(name), 200
+    else:
+        return "not found", 404
+
 
 #start development server using the run() method
 if __name__ == "__main__":
